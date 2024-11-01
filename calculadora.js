7919c2b4a089b0874417d7154d3f9bc6f05ef873
genisbou <genisbou@gmail.com>
2024-11-01 19:07:58 +0100

    Afegir JS
    OK

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+let numero1 = "";
+let numero2 = "";
+let operador = "";
+let resultat = "";
+
+// Seleccionar tots els components que tinguin el ID display
+const display = document.getElementById("display");
+// Seleccionar tots els type button 
+const buttons = document.querySelectorAll("button");
+
+
