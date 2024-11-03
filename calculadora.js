899422e4ddbfdd930567e43297e0f7703bcc724c
genisbou <genisbou@gmail.com>
2024-11-03 12:03:21 +0100

    DEL

--- calculadora.js
+++ calculadora.js
@@ -17,17 +17,25 @@ buttons.forEach (button => {
             display.value = buttonselect;  // Si el valor és 0, es mostra directament
         }
 
-        if  (buttonselect === "C") {
+        if  (button.id === "clear") {
             display.value = "0" ; // Si el valor és C, es posa a 0
+            return; // Si esborra, no fa res més (no continua)
         }
         
-        if (buttomselect === "del") {
-            display.value = display.value.slice(0, -1); // Si el valor és del, esborra l'últim caràcter
+        if (button.id === "del") {
+            // Si la length del display és 1, posa el valor a 0
+            if (display.value.length === 1) {
+                display.value = "0";
+            }
+            else{ // Si no, esborra l'últim caràcter
+                display.value = display.value.slice(0, -1);
+                return; //  Si esborra, no fa res més (no continua)
+            }
         }
+        
         else {
             display.value += buttonselect;  // Si no és 0, s'afegeix al valor existent
         }
-        
         
     });
 });
