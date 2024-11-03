2042d2986aae3d3c24dbb2a4054d9f457306b8a9
genisbou <genisbou@gmail.com>
2024-11-03 11:38:26 +0100

    OK

--- calculadora.js
+++ calculadora.js
@@ -5,7 +5,7 @@ let resultat = "";
 
 // Seleccionar tots els components que tinguin el ID display
 const display = document.getElementById("display");
-// Seleccionar tots els type button 
+// Seleccionar tots els type button amb classe button
 const buttons = document.querySelectorAll(".button"); // Crea una array
 
 buttons.forEach (button => {
@@ -16,11 +16,14 @@ buttons.forEach (button => {
         if (display.value === "0") { 
             display.value = buttonselect;  // Si el valor és 0, es mostra directament
         }
-        
+
         if  (buttonselect === "C") {
             display.value = "0" ; // Si el valor és C, es posa a 0
         }
         
+        if (buttomselect === "del") {
+            display.value = display.value.slice(0, -1); // Si el valor és del, esborra l'últim caràcter
+        }
         else {
             display.value += buttonselect;  // Si no és 0, s'afegeix al valor existent
         }
