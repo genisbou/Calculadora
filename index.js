31b5f610c087e629ffc4793bee4f8984b941142f
genisbou <genisbou@gmail.com>
2024-11-09 10:42:07 +0100

    Language
    OK

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const browserInfo = {
     browserName: navigator.userAgent.match(/(Chrome|Firefox|Safari|Edge|Opera)/i)[0],
     browserVersion: navigator.userAgent.match(/(\d+(?:\.\d+)+)/)[0],
-    os: navigator.platform
-  };
+    os: navigator.platform,
+    lang : navigator.language,
+  
+};
   
   // Mostrar la informació en el document HTML
   document.getElementById("browser-info").innerHTML = `
     <p>Navegador: ${browserInfo.browserName}</p>
-    <p>Versión: ${browserInfo.browserVersion}</p>
-    <p>Sistema Operativo: ${browserInfo.os}</p>
+    <p>Versió: ${browserInfo.browserVersion}</p>
+    <p>Sistema Operatiu: ${browserInfo.os}</p>
   `;
