c3906e61d9f87906e551425331641567d7b8d95c
genisbou <genisbou@gmail.com>
2024-11-09 13:46:04 +0100

    Tipus calculadora
    OK

--- index.js
+++ index.js
@@ -37,7 +37,25 @@ document.getElementById("go-back").addEventListener("click", goBack);
 // Go forward button
 document.getElementById("go-forward").addEventListener("click", goForward);
 
-// Si es prem el boto settings, preguntar nom usuariç
+// Si es prem el boto settings, preguntar nom usuari i tipus de calculadora i guardar-ho
 document.getElementById("settings").addEventListener("click", () => {
     let username = prompt("Introdueix el teu nom d'usuari");
+    // Guardar el nom d'usuari a localStorage
+    localStorage.setItem("username", username);
+    // Guardar el tipus de calculadora a localStorage
+    let calculatorType = prompt("Introdueix el tipus de calculadora (1: Bàsica, 2: Científica)");
+    localStorage.setItem("calculatorType", calculatorType);
+    alert("Configuració guardada correctament. En el botó de calculadora pots veure el tipus de calculadora que has seleccionat.");
+});
+// Si es prem el boto de calculadora, mostrar el tipus de calculadora que s'ha guardat
+document.getElementById("calculator").addEventListener("click", () => {
+    let username = localStorage.getItem("username");
+    let calculatorType = localStorage.getItem("calculatorType");
+    alert(`Nom d'usuari: ${username}\nTipus de calculadora (1: Bàsica, 2: Científica): ${calculatorType}`);
+    if (calculatorType == 1) {
+        window.location.href = "calculadora.html";
+    }
+    if (calculatorType == 2) {
+        window.location.href = "calculadora_cientifica.html";
+    }
 });
