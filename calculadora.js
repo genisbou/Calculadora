1ce15df2b6545b41df1703c79b835da1d0d6d27a
genisbou <genisbou@gmail.com>
2024-11-10 17:25:23 +0100

    OK
    OK

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,7 @@
 let numero1 = "";
 let numero2 = "";
 let operador = "";
-let error1= ("ERROR no es pot dividir/0.");
+let error1= ("ERROR no es pot dividir per 0.");
 
 
 // Seleccionar tots els components que tinguin el ID display
@@ -33,15 +33,49 @@ buttons.forEach (button => {
         }
 
         if (button.id === "igual") {
-            
            
+
             const numeros_splitats = display.value.split(operador); //Separa els valors per l'operador
             numero1 = (parseFloat(numeros_splitats[0])); // Guarda el primer valor a numero1
             numero2= (parseFloat(numeros_splitats[1])); // Guarda el segon valor a numero2
-        
+            // Mostra arrel quadrada
+            if (operador === "√") {
+                display.value = resultat_arrel; 
+                return;
+            }
+            // Mostra l'exponent
+            if (operador === "^") {
 
-        if (button.id == "suma"){
+                display.value = Math.pow(parseFloat(numero1), parseFloat(numero2));
+                
+                return;
+            }
+            display.value = eval(parseFloat(numero1) + operador + parseFloat(numero2)); // Evalua la expressió
+
             
+           if (operador === "/" && numero2 === 0) {
+                display.value = error1;
+                return;
+            }
+            
+            if (biutton.id === "divisio") {
+
+                numero1 = display.value; // Guarda el resultat a numero1
+                numero2 = ""; // Esborra el valor de numero2
+                operador = ""; // Esborra el valor de operador
+
+                return; // Si és igual, no fa res més (no continua)
+            }
+
+
+            
+           
+           
+
+        }
+
+        if (button.id === "suma"){
+
             numero1 = display.value; // Guarda el valor del display a numero1
             operador = "+"; // Guarda el valor de l'operador
             display.value = operador;
@@ -49,11 +83,11 @@ buttons.forEach (button => {
             display.value += operador;
             // Veure tota la operaico a la pantalla abans de fer la suma
             return; // Si és suma, no fa res més (no continua)
-            
+
         }
 
-        if (button.id == "resta"){
-            
+        if (button.id === "resta"){
+
             numero1 = display.value; // Guarda el valor del display a numero1
             operador = "-"; // Guarda el valor de l'operador
             display.value = operador;
@@ -64,7 +98,7 @@ buttons.forEach (button => {
 
         }
 
-        if (button.id == "multiplicacio"){
+        if (button.id === "multiplicacio"){
 
             numero1 = display.value; // Guarda el valor del display a numero1
             operador = "*"; // Guarda el valor de l'operador
@@ -75,20 +109,20 @@ buttons.forEach (button => {
             return; // Si és suma, no fa res més (no continua)
         }
 
-        
+        if (button.id === "divisio"){
 
-        if (button.id == "divisio" ){
-
-            numero1 = display.value; // Guarda el valor del display a numero1
+                numero1 = display.value; // Guarda el valor del display a numero1
                 operador = "/"; // Guarda el valor de l'operador
                 display.value = operador;
                 display.value = numero1;
                 display.value += operador;
                 // Veure tota la operaicó a la pantalla abans de fer la suma
                 return; // Si és suma, no fa res més (no continua)
+           
+
         }
         
-    }         
+                       
 
         if (display.value === "0") { 
             display.value = buttonselect;  // Si el valor és 0, es mostra directament
